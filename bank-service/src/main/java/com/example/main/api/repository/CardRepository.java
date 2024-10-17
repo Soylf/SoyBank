258d37c42ae953055dc56ee9d88dfb16be46aa13
Soyl <Soylgr@yandex.ru>
2024-10-17 20:29:59 +0300

    New test

--- bank-service/src/main/java/com/example/main/api/repository/CardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.main.api.repository;
-
-import main.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-    Card findByCardId(String kode);
-}
--- user-service/src/main/java/com/example/main/api/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.main.api.repository;
-
-import main.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
